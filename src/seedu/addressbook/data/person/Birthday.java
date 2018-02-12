468ec199f8e91e700c8fad6e9e6edbed123fc680
tinyjy94 <tiny_jy@hotmail.com>
2018-02-12 21:34:24 +0800

    try reset test

---
+++ src/seedu/addressbook/data/person/Birthday.java
@@ -0,0 +1,65 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ * Represents a Person's birthday in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidBirthday(String)}
+ */
+public class Birthday {
+
+    public static final String EXAMPLE = "12-03-1990";
+    public static final String MESSAGE_BIRTHDAY_CONSTRAINTS =
+            "Person birthday in format DD-MM-YYYY where DD is days and MM is months and YYYY is years";
+    public static final String BIRTHDAY_VALIDATION_REGEX = "(((0[1-9]|[12]\\d|3[01])[-](0[13578]|1[02])[-]((1[6-9]|"
+                + "[2-9]\\d)\\d{2}))|((0[1-9]|[12]\\d|30)[-](0[13456789]|1[012])[-]((1[6-9]|[2-9]\\d)\\d{2}))"
+                + "|((0[1-9]|1\\d|2[0-8])[-]02[-]((1[6-9]|[2-9]\\d)\\d{2}))|(29[-]02[-]((1[6-9]|[2-9]\\d)"
+                + "(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))";
+
+    public final String value;
+    private boolean isPrivate;
+
+    /**
+     * Validates given birthday.
+     *
+     * @throws IllegalValueException if given birthday string is invalid.
+     */
+    public Birthday(String birthday, boolean isPrivate) throws IllegalValueException {
+        this.isPrivate = isPrivate;
+        String trimmedBirthday = birthday.trim();
+        if (!isValidBirthday(trimmedBirthday)) {
+            throw new IllegalValueException(MESSAGE_BIRTHDAY_CONSTRAINTS);
+        }
+        this.value = trimmedBirthday;
+    }
+
+    /**
+     * Returns true if the given string is a valid person birthday.
+     */
+    public static boolean isValidBirthday(String test) {
+        return test.matches(BIRTHDAY_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Birthday // instanceof handles nulls
+                && this.value.equals(((Birthday) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
+
