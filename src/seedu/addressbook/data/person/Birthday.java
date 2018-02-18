9514b3c7cc16372bf9d2f777585938bcc0406077
tinyjy94 <tiny_jy@hotmail.com>
2018-02-18 13:11:39 +0800

    Modify birthday validation regex to multiple regex

--- src/seedu/addressbook/data/person/Birthday.java
+++ src/seedu/addressbook/data/person/Birthday.java
@@ -11,10 +11,10 @@ public class Birthday {
     public static final String EXAMPLE = "12-03-1990";
     public static final String MESSAGE_BIRTHDAY_CONSTRAINTS =
             "Person birthday in format DD-MM-YYYY where DD is days and MM is months and YYYY is years";
-    public static final String BIRTHDAY_VALIDATION_REGEX = "(((0[1-9]|[12]\\d|3[01])[-](0[13578]|1[02])[-]((1[6-9]|"
-                + "[2-9]\\d)\\d{2}))|((0[1-9]|[12]\\d|30)[-](0[13456789]|1[012])[-]((1[6-9]|[2-9]\\d)\\d{2}))"
-                + "|((0[1-9]|1\\d|2[0-8])[-]02[-]((1[6-9]|[2-9]\\d)\\d{2}))|(29[-]02[-]((1[6-9]|[2-9]\\d)"
-                + "(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))";
+    public static final String THIRTY_ONE_DAYS_REGEX = "((0[1-9]|[12]\\d|3[01])[-](0[13578]|1[02])[-]((19|[2-9]\\d)\\d{2}))";
+    public static final String THIRTY_DAYS_REGEX = "((0[1-9]|[12]\\d|30)[-](0[469]|11)[-]((19|[2-9]\\d)\\d{2}))";
+    public static final String LEAP_YEAR_REGEX = "(29[-]02[-]((1[6-9]|[2-9]\\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)))";
+    public static final String NON_LEAP_REGEX = "((0[1-9]|1\\d|2[0-8])[-]02[-]((19|[2-9]\\d)\\d{2}))";
 
     public final String value;
     private boolean isPrivate;
@@ -37,7 +37,9 @@ public class Birthday {
      * Returns true if the given string is a valid person birthday.
      */
     public static boolean isValidBirthday(String test) {
-        return test.matches(BIRTHDAY_VALIDATION_REGEX);
+        return test.matches(THIRTY_ONE_DAYS_REGEX) || test.matches(THIRTY_DAYS_REGEX)
+                                                   || test.matches(LEAP_YEAR_REGEX)
+                                                   || test.matches(NON_LEAP_REGEX);
     }
 
     @Override
